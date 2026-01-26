3ff0e080579676fcef3858ce5670dd372d5cfd5e
Jou Okuyama <ojoxux@gmail.com>
2026-01-26 21:20:03 +0900

    use official template

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,7 @@
-const config = {}
+import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
 
-export default config
+export default defineCloudflareConfig({
+  incrementalCache: r2IncrementalCache,
+})
 
